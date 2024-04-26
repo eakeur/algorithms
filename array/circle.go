418d25dbdf9f4eda6afb9bdfadfafd214dc6a5e5
Igor Reis <63017440+eakeur@users.noreply.github.com>
2024-04-26 14:14:43 -0300

    feat: improve docs

--- array/circle.go
+++ array/circle.go
@@ -1,6 +1,9 @@
 package array
 
 // getCycle iterates over a slice s times, reading back to the beginning if len(a) < s
+// Given a=[1, 2, 3, 4], start=3 and s=2, the result will be [4, 1].
+// As we start reading the array from the position 3, we go back to the
+// beginning everytime s >= len(a)
 func getCycle(a []int, start int, s int) []int {
 	n := len(a)
 	if n == 0 {
